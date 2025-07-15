3a1ca408c470a8deb2a2ffa4e6f04afd0a6325fb
DevDangel <angeldavidh18@gmail.com>
2025-07-15 14:39:33 -0500

    prueba

--- Main2.java
+++ Main2.java
@@ -2,4 +2,8 @@ public class Main2 {
     public static void main(String[] args) {
         System.out.println("HELL WORLD");
     }
+
+    public void saludar(){
+        System.out.println("Hola perros");
+    }
 }
